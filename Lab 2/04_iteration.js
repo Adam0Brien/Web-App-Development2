5e760469e60447f7df05cf3acddee0f74ba68750
Adam O'Brien <74919798+Adam0Brien@users.noreply.github.com>
2022-09-22 13:02:23 +0100

    Add files via upload

---
+++ Lab 2/04_iteration.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
+
+const Demo = props => {
+  const list = props.modules.map((module, index) => {
+    return (
+      <tr key={index}>
+            <td>{props.modules[index].name}</td>
+            <td>{props.modules[index].noLectures}</td>
+            <td>{props.modules[index].noPracticals}</td>
+      </tr>
+    );
+  });
+  return (
+    <>
+      <h2>{props.course}</h2>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>No lectures</th>
+            <th>No practicals</th>
+          </tr>
+        </thead>
+        <tbody>{list}</tbody>
+      </table>
+    </>
+  );
+};
+
+export default Demo;
